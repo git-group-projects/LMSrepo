f7b3df8a5b69d73f07ae2e4a2cebf144370a0aa1
gitLeii <sfarruki@gmail.com>
2021-06-30 14:19:45 +0545

    changes made on post model and database

--- LMS/dashboard/admin.py
+++ LMS/dashboard/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import Post, BlogComment
 
 # Register your models here.
-admin.site.register((Post, BlogComment))
+admin.site.register(Post)
+admin.site.register(BlogComment)
--- LMS/dashboard/models.py
+++ LMS/dashboard/models.py
@@ -3,9 +3,17 @@ from django.utils.timezone import now
 from django.contrib.auth.models import  User
 # Create your models here.
 
-class Post(models.Model):  
-    pass
 
+class Post(models.Model):  
+    sno = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.CharField(max_length=25)
+    timestamp = models.DateTimeField(blank=True)
+
+    def __str__(self):
+        return self.title + ' ' +self.author
+    
 
 class BlogComment(models.Model):
     sno = models.AutoField(primary_key=True)
@@ -13,4 +21,4 @@ class BlogComment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post,on_delete=models.CASCADE)
     parent = models.ForeignKey('self', on_delete=models.CASCADE,null=True)
-    timestamp = models.DateTimeField(default=now)
+    timestamp = models.DateTimeField(default=now) 
